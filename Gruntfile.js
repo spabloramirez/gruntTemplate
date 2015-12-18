b1e27b4233d783e7a637b0a38b1401d1ce491294
Pablo Ramirez <pramirez@intermundial.es>
2015-12-18 10:46:33 +0100

    task-folder

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,15 +1,26 @@
 module.exports = function(grunt) {
+    /**
+     * http://thenittygritty.co/shared-grunt-configuration
+     * Load configuration files for Grunt
+     * @param  {string} path Path to folder with tasks
+     * @return {object}      All options
+     */
+    var loadConfig = function (path) {
+        var glob = require('glob');
+        var object = {};
+        var key;
 
-    // Project configuration.
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-        uglify: {
-            build: {
-                src: ['js/global.js','js/test.js'],
-                dest: 'js/dist/global.min.js'
-            }
-        }
-    });
+        glob.sync('*', { cwd: path }).forEach(function (option) {
+            key = option.replace(/\.js$/,'');
+            object[key] = require(path + option);
+        });
+
+        return object;
+    };
+
+    var config = {pkg: grunt.file.readJSON('package.json')};
+    grunt.util._.extend(config, loadConfig('./tasks/options/'));
+    grunt.initConfig(config);
 
     // Load the plugin that provides the "uglify" task.
     grunt.loadNpmTasks('grunt-contrib-uglify');
---
+++ tasks/options/uglify.js
@@ -0,0 +1,6 @@
+module.exports = {
+  build: {
+    src: ['js/global.js','js/test.js'],
+    dest: 'js/dist/global.min.js'
+  }
+}
